f4841df91747179e23989bc46135e1915b4e7cad
Mike <binarybardcode@gmail.com>
2025-06-22 00:10:11 +0200

    feat: add Hero & Navbar with GSAP scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className=''>
-        
-    </div>
+    <main>
+        <Navbar />
+        <Hero />
+    </main>
   )
 }
 
