7472d4ece5ee4de5780cf0f3dae02b266a6f804d
Mike <binarybardcode@gmail.com>
2025-06-22 22:17:22 +0200

    feat: add animated About, Art, Cocktails, Contact, and Menu components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,6 +15,11 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
+        <Cocktails />
+        <About />
+        <Art />
+        <Menu />
+        <Contact />
     </main>
   )
 }
